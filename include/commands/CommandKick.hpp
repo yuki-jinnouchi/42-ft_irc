2602d569dbec6a5ee18d66d4a6b1abc6dda3b047
Susumu Yagi <susumu.yagi@gmail.com>
2025-06-06 09:05:04 +0000

    implimented CommandKick

---
+++ include/commands/CommandKick.hpp
@@ -0,0 +1,115 @@
+#pragma once
+#ifndef __COMMAND_KICK_HPP__
+#define __COMMAND_KICK_HPP__
+
+#include "ACommand.hpp"
+
+class CommandKick : public ACommand {
+ public:
+  // Orthodox Canonical Form
+  CommandKick(IRCServer* server);
+  ~CommandKick();
+
+  // Member functions
+  void execute(IRCMessage& msg);
+
+ private:
+  CommandKick();
+  CommandKick(const CommandKick& other);
+  CommandKick& operator=(const CommandKick& other);
+
+  bool validateKick(IRCMessage& msg);
+  Channel* getAndValidateChannel(IRCMessage& msg, const std::string& ch_str);
+  Client* getAndValidateClient(IRCMessage& msg, Channel* channel,
+                               const std::string& nick);
+
+  void kickMembers(IRCMessage& msg, Channel* channel,
+                   const std::vector<std::string> nicks);
+};
+
+#endif  // __COMMAND_KICK_HPP__
+
+/*
+https://www.rfc-editor.org/rfc/rfc1459#section-4.2.8
+4.2.8 Kick command
+
+      Command: KICK
+   Parameters: <channel> <user> [<comment>]
+
+   The KICK command can be  used  to  forcibly  remove  a  user  from  a
+   channel.   It  'kicks  them  out'  of the channel (forced PART).
+   Only a channel operator may kick another user out of a  channel.
+   Each  server that  receives  a KICK message checks that it is valid
+   (ie the sender is actually a  channel  operator)  before  removing
+   the  victim  from  the channel.
+
+   Numeric Replies:
+
+           ERR_NEEDMOREPARAMS              ERR_NOSUCHCHANNEL
+           ERR_BADCHANMASK                 ERR_CHANOPRIVSNEEDED
+           ERR_NOTONCHANNEL
+
+   Examples:
+
+KICK &Melbourne Matthew         ; Kick Matthew from &Melbourne
+
+KICK #Finnish John :Speaking English
+                                ; Kick John from #Finnish using
+                                "Speaking English" as the reason
+                                (comment).
+
+:WiZ KICK #Finnish John         ; KICK message from WiZ to remove John
+                                from channel #Finnish
+
+NOTE:
+     It is possible to extend the KICK command parameters to the
+following:
+
+<channel>{,<channel>} <user>{,<user>} [<comment>]
+
+// 通常（コメントなし）
+KICK #ch nick2
+:nick1!~a@localhost KICK #ch nick2 :nick1
+
+// 通常(コメントあり)
+KICK #ch1 nick2 bye
+:nick1!~a@localhost KICK #ch1 nick2 :bye
+
+// ログインしていない
+:irc.example.net 451 * :Connection not registered
+
+// 引数が足りない
+// 引数が多い
+KICK #ch1 nick2 a b
+:irc.example.net 461 nick1 KICK :Syntax error
+
+// チャンネルに所属していない
+KICK #ch1 nick2
+:irc.example.net 442 nick1 #ch1 :You are not on that channel
+
+//　権限がない（チャンネルのオペレーターではない）
+KICK #ch1 nick1
+:irc.example.net 482 nick2 #ch1 :Your privileges are too low
+
+// チャンネルがない
+KICK #chx nick3 bye
+:irc.example.net 403 nick1 #chx :No such channel
+// ニックネームがない
+KICK #ch1 nickx bye
+:irc.example.net 401 nick1 nickx :No such nick or channel name
+
+// ユーザーがチャンネルにいない
+KICK #ch2 nick3 bye
+:irc.example.net 441 nick1 nick3 #ch2 :They aren't on that channel
+
+// 自分自身はKICKできる？ 可能
+// operatorをKICKできる？　可能
+
+
+           ERR_NEEDMOREPARAMS 461
+        ERR_NOSUCHCHANNEL 403
+           ERR_BADCHANMASK
+          ERR_CHANOPRIVSNEEDED 482
+           ERR_NOTONCHANNEL 442
+
+*/
---
+++ test/unit/src/Commands/TestCommandKick.cpp
@@ -0,0 +1,245 @@
+#include <gtest/gtest.h>
+
+#include "TestDataGenerator.hpp"
+
+// 通常（コメントあり）
+TEST(CommandKick, nomal1) {
+  IRCServer server("6677", "pass123");
+  std::map<int, Client *> clients;
+  RequestHandler requestHandler(&server);
+  TestDataGenerator::makeUserData(server, clients, requestHandler);
+
+  std::string msgStr = "KICK #ch3 nick2 :bye bye";
+  std::string expected = ":nick1!~user1@localhost KICK #ch3 nick2 :bye bye";
+
+  IRCMessage msg(clients[10], msgStr);
+  requestHandler.handleCommand(msg);
+
+  // メンバーが見つからない
+  std::set<Client *> member = server.getChannel("#ch3")->getMember();
+  EXPECT_EQ(member.find(clients[11]), member.end());
+
+  EXPECT_EQ(clients[10]->getSendingMsg(), expected + "\r\n");
+  // TODO チャンネルの他のユーザーへの通知をテストする
+  EXPECT_EQ(clients[11]->getSendingMsg(), expected + "\r\n");
+  EXPECT_EQ(clients[12]->getSendingMsg(), expected + "\r\n");
+
+  EXPECT_EQ(clients[13]->getSendingMsg(), "");
+}
+
+// 通常(コメントなし)
+TEST(CommandKick, nomal2) {
+  IRCServer server("6677", "pass123");
+  std::map<int, Client *> clients;
+  RequestHandler requestHandler(&server);
+  TestDataGenerator::makeUserData(server, clients, requestHandler);
+
+  std::string msgStr = "KICK #ch3 nick2";
+  std::string expected = ":nick1!~user1@localhost KICK #ch3 nick2 :nick1";
+
+  IRCMessage msg(clients[10], msgStr);
+  requestHandler.handleCommand(msg);
+
+  // メンバーが見つからない
+  std::set<Client *> member = server.getChannel("#ch3")->getMember();
+  EXPECT_EQ(member.find(clients[11]), member.end());
+
+  EXPECT_EQ(clients[10]->getSendingMsg(), expected + "\r\n");
+  // TODO チャンネルの他のユーザーへの通知をテストする
+  EXPECT_EQ(clients[11]->getSendingMsg(), expected + "\r\n");
+  EXPECT_EQ(clients[12]->getSendingMsg(), expected + "\r\n");
+
+  EXPECT_EQ(clients[13]->getSendingMsg(), "");
+}
+
+// ログインしていない
+TEST(CommandKick, not_registered) {
+  IRCServer server("6677", "pass123");
+  std::map<int, Client *> clients;
+  RequestHandler requestHandler(&server);
+  TestDataGenerator::makeUserData(server, clients, requestHandler);
+
+  std::string msgStr = "KICK #ch3 nick2";
+  std::string expected = ":irc.example.net 451 * :You have not registered";
+
+  IRCMessage msg(clients[15], msgStr);
+  requestHandler.handleCommand(msg);
+
+  // エラーメッセージ
+  EXPECT_EQ(clients[15]->getSendingMsg(), expected + "\r\n");
+  // 何も送信されない
+  EXPECT_EQ(clients[11]->getSendingMsg(), "");
+  EXPECT_EQ(clients[12]->getSendingMsg(), "");
+}
+
+// 引数エラー //////////////////////////////////////////////////////////
+
+// 引数なし
+TEST(CommandKick, no_arg) {
+  IRCServer server("6677", "pass123");
+  std::map<int, Client *> clients;
+  RequestHandler requestHandler(&server);
+  TestDataGenerator::makeUserData(server, clients, requestHandler);
+
+  std::string msgStr = "KICK";
+  std::string expected =
+      ":irc.example.net 461 nick1 KICK :Not enough parameters";
+
+  IRCMessage msg(clients[10], msgStr);
+  requestHandler.handleCommand(msg);
+
+  // エラーメッセージ
+  EXPECT_EQ(clients[10]->getSendingMsg(), expected + "\r\n");
+  // 何も送信されない
+  EXPECT_EQ(clients[11]->getSendingMsg(), "");
+  EXPECT_EQ(clients[12]->getSendingMsg(), "");
+}
+
+// 引数不足
+TEST(CommandKick, arg1) {
+  IRCServer server("6677", "pass123");
+  std::map<int, Client *> clients;
+  RequestHandler requestHandler(&server);
+  TestDataGenerator::makeUserData(server, clients, requestHandler);
+
+  std::string msgStr = "KICK #ch3";
+  std::string expected =
+      ":irc.example.net 461 nick1 KICK :Not enough parameters";
+
+  IRCMessage msg(clients[10], msgStr);
+  requestHandler.handleCommand(msg);
+
+  // エラーメッセージ
+  EXPECT_EQ(clients[10]->getSendingMsg(), expected + "\r\n");
+  // 何も送信されない
+  EXPECT_EQ(clients[11]->getSendingMsg(), "");
+  EXPECT_EQ(clients[12]->getSendingMsg(), "");
+}
+
+// 引数過剰
+TEST(CommandKick, arg4) {
+  IRCServer server("6677", "pass123");
+  std::map<int, Client *> clients;
+  RequestHandler requestHandler(&server);
+  TestDataGenerator::makeUserData(server, clients, requestHandler);
+
+  std::string msgStr = "KICK #ch3 nick2 bye bye";
+  std::string expected =
+      ":irc.example.net 461 nick1 KICK :Not enough parameters";
+
+  IRCMessage msg(clients[10], msgStr);
+  requestHandler.handleCommand(msg);
+
+  // エラーメッセージ
+  EXPECT_EQ(clients[10]->getSendingMsg(), expected + "\r\n");
+  // 何も送信されない
+  EXPECT_EQ(clients[11]->getSendingMsg(), "");
+  EXPECT_EQ(clients[12]->getSendingMsg(), "");
+}
+
+// チャンネルエラー //////////////////////////////////////////////////////////
+
+// チャンネルが存在しない
+TEST(CommandKick, not_exit_ch) {
+  IRCServer server("6677", "pass123");
+  std::map<int, Client *> clients;
+  RequestHandler requestHandler(&server);
+  TestDataGenerator::makeUserData(server, clients, requestHandler);
+
+  std::string msgStr = "KICK #chx nick2 :bye bye";
+  std::string expected = ":irc.example.net 403 nick1 #chx :No such channel";
+
+  IRCMessage msg(clients[10], msgStr);
+  requestHandler.handleCommand(msg);
+
+  // エラーメッセージ
+  EXPECT_EQ(clients[10]->getSendingMsg(), expected + "\r\n");
+  // 何も送信されない
+  EXPECT_EQ(clients[11]->getSendingMsg(), "");
+  EXPECT_EQ(clients[12]->getSendingMsg(), "");
+}
+
+// チャンネルに所属していない
+TEST(CommandKick, I_am_not_member) {
+  IRCServer server("6677", "pass123");
+  std::map<int, Client *> clients;
+  RequestHandler requestHandler(&server);
+  TestDataGenerator::makeUserData(server, clients, requestHandler);
+
+  std::string msgStr = "KICK #ch4 nick2 :bye bye";
+  std::string expected =
+      ":irc.example.net 442 nick1 #ch4 :You're not on that channel";
+
+  IRCMessage msg(clients[10], msgStr);
+  requestHandler.handleCommand(msg);
+
+  // エラーメッセージ
+  EXPECT_EQ(clients[10]->getSendingMsg(), expected + "\r\n");
+  // 何も送信されない
+  EXPECT_EQ(clients[11]->getSendingMsg(), "");
+  EXPECT_EQ(clients[12]->getSendingMsg(), "");
+}
+
+// 　権限がない（チャンネルのオペレーターではない）
+TEST(CommandKick, no_operator) {
+  IRCServer server("6677", "pass123");
+  std::map<int, Client *> clients;
+  RequestHandler requestHandler(&server);
+  TestDataGenerator::makeUserData(server, clients, requestHandler);
+
+  std::string msgStr = "KICK #ch3 nick2 :bye bye";
+  std::string expected =
+      ":irc.example.net 482 nick2 #ch3 :You're not channel operator";
+
+  IRCMessage msg(clients[11], msgStr);
+  requestHandler.handleCommand(msg);
+
+  // エラーメッセージ
+  EXPECT_EQ(clients[11]->getSendingMsg(), expected + "\r\n");
+  // 何も送信されない
+  EXPECT_EQ(clients[10]->getSendingMsg(), "");
+  EXPECT_EQ(clients[12]->getSendingMsg(), "");
+}
+
+// クライアントエラー //////////////////////////////////////////////////////////
+
+// ニックネームが存在しない
+TEST(CommandKick, not_exit_nick) {
+  IRCServer server("6677", "pass123");
+  std::map<int, Client *> clients;
+  RequestHandler requestHandler(&server);
+  TestDataGenerator::makeUserData(server, clients, requestHandler);
+
+  std::string msgStr = "KICK #ch3 xxx :bye bye";
+  std::string expected = ":irc.example.net 401 nick1 xxx :No such nick/channel";
+
+  IRCMessage msg(clients[10], msgStr);
+  requestHandler.handleCommand(msg);
+
+  // エラーメッセージ
+  EXPECT_EQ(clients[10]->getSendingMsg(), expected + "\r\n");
+  // 何も送信されない
+  EXPECT_EQ(clients[11]->getSendingMsg(), "");
+  EXPECT_EQ(clients[12]->getSendingMsg(), "");
+}
+
+// 対象者がチャンネルに参加していない
+TEST(CommandKick, nick_is_not_member) {
+  IRCServer server("6677", "pass123");
+  std::map<int, Client *> clients;
+  RequestHandler requestHandler(&server);
+  TestDataGenerator::makeUserData(server, clients, requestHandler);
+
+  std::string msgStr = "KICK #ch3 nick4 :bye bye";
+  std::string expected =
+      ":irc.example.net 441 nick1 nick4 #ch3 :They aren't on that channel";
+
+  IRCMessage msg(clients[10], msgStr);
+  requestHandler.handleCommand(msg);
+
+  // エラーメッセージ
+  EXPECT_EQ(clients[10]->getSendingMsg(), expected + "\r\n");
+  // 何も送信されない
+  EXPECT_EQ(clients[11]->getSendingMsg(), "");
+  EXPECT_EQ(clients[12]->getSendingMsg(), "");
+}
